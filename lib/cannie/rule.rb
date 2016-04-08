d63b6005de1c60510ead11b487efd355bebe62aa
hck <alexsvirin@gmail.com>
2016-04-08 09:52:22 +0200

    Add RDoc/Yard comments to public interface.

--- lib/cannie/rule.rb
+++ lib/cannie/rule.rb
@@ -2,15 +2,29 @@ module Cannie
   class Rule
     attr_reader :action, :subject
 
-    def initialize(action, subject, options={})
+    # Initializes new rule
+    #
+    # @param [Symbol] action action which should be permitted on subject
+    # @param [String, Symbol] subject subject of the rule
+    # @param [Hash] options additional options and conditions for the new rule
+    # @option options [Proc] :if condition which is checked for a particular Permissions object
+    #                            and should be evaluated to true
+    # @option options [Proc] :unless condition which is checked for a particular Permissions object
+    #                                and should be evaluated to false
+    def initialize(action, subject, options = {})
       @action, @subject, @_if, @_unless = action, subject, *options.values_at(:if, :unless)
     end
 
+    # Checks whether rule is applied to permissions passed as an argument
+    #
+    # @param [Cannie::Permissions] permissions
+    # @return [Boolean]
     def applies_to?(permissions)
       if?(permissions) && unless?(permissions)
     end
 
     private
+
     attr_reader :_if, :_unless
 
     def if?(permissions)
